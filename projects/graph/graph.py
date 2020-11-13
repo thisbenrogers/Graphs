6caaf63fbaeafa47f5be9ba0a04cdff16a6dbc07
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-11-12 22:56:05 -0600

    Adds pseudocode in comment block

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -170,6 +170,27 @@ class Graph:
         depth-first order.
 
         This should be done using recursion.
+
+        Psuedocode below
+
+        DFS(graph):
+            for v of graph.verts:
+                v.color = white
+                v.parent = null
+
+            for v of graph.verts:
+                if v.color == white:
+                    DFS_visit(v)
+
+        DFS_visit(v):
+            v.color = gray
+
+            for neighbor of v.adjacent_nodes:
+                if neighbor.color == white:
+                    neighbor.parent = v
+                    DFS_visit(neighbor)
+
+            v.color = black
         """
         
 
