dc263b3e72ef66a10be305714b2dcf716737a284
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-11-18 11:07:33 -0600

    Completes answers to study questions

--- projects/social/social.py
+++ projects/social/social.py
@@ -131,7 +131,7 @@ class SocialGraph:
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populate_graph(1000, 5)
+    sg.populate_graph(10, 2)
     print(sg.friendships)
     connections = sg.get_all_social_paths(1)
     print(connections)
