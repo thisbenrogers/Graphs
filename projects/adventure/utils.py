5abfed7166f7bccfe73e3b2f1d0c46f15efb7111
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-11-27 12:58:58 -0600

    Adds Stack and Queue utilities

---
+++ projects/adventure/utils.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+class Queue():
+    def __init__(self):
+        self.queue = deque()
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.popleft()
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
