7a241503f68fcd1e940976f905adc4b65ef24f09
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-11-27 20:57:53 -0600

    Traverses test_line map

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -11,8 +11,8 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+# map_file = "maps/test_line.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
@@ -41,43 +41,46 @@ class Traversal_Graph():
         self.rooms[room_id] = {}
         return None
 
+    def show_exits(self, room_id):
+        return self.rooms[room_id]
+
+    def move_player(self, cardinal_direction):
+        player.travel(cardinal_direction)
+        traversal_path.append(cardinal_direction)
+        return None
+
     def get_opposite(self, cardinal_direction):
+        # * for use in self.update_exit()
         opposite = {
             'n': 's',
             's': 'n',
             'e': 'w',
             'w': 'e'
         }
-        return oppostie[cardinal_direction]
+        return opposite[cardinal_direction]
 
     def add_unexplored_exits(self):
-        # player mus be currently in the room 
+        #   player must be currently in the room 
         # ! assumes that room is being visited for the 1st time
-        # this function adds ALL exits, 
+        #   this function adds ALL exits, 
         room_id = player.current_room.id
         anon_exits = player.current_room.get_exits()
         for ex in anon_exits:
-            self.rooms[room_id][ex] = '?'
+            if ex not in self.rooms[room_id].keys():
+                self.rooms[room_id][ex] = '?'
         return None
 
     def update_exit(self, rm1_id, direction, rm2_id):
-        # call this function AFTER player has traveled to new unexplored exit
+        # ! call this function AFTER player has traveled to new unexplored exit
         self.rooms[rm1_id][direction] = rm2_id
-        self.add_room(rm2_id)
+        if rm2_id not in self.visited:
+            self.add_room(rm2_id)
         opposite_dir = self.get_opposite(direction)
         self.rooms[rm2_id][opposite_dir] = rm1_id
         return None
 
-    def show_exits(self, room_id):
-        return self.rooms[room_id]
-
-    def move_player(self, direction):
-        player.travel(direction)
-        traversal_path.append(direction)
-        return None
-
-    def get_rand_unexplored_dir(self, dir_arr):
-        # *     returns a string with one cardinal direction chosen from the passed-in array
+    def get_rand_unexplored_dir(self):
+        # *     returns a string with one cardinal direction from CURRENT ROOM
         # *     returns None if dead-end
         rand_arr = []
         room_id = player.current_room.id
@@ -89,22 +92,52 @@ class Traversal_Graph():
         return random.choice(rand_arr)
 
     def find_nearest_unexplored(self):
-        # * returns {room_id: {'direction': '?'}} once unexplored direction is found, or
-        # * returns None if there are no unexplored directions left
+        # *     moves player (retracing steps) until in a room with unexplored exit
+        # *     uses bfs to find nearest unexplored exit
+        # *     returns {room_id: {'direction': '?'}} once unexplored direction is found, or
+        # *     returns None if there are no unexplored directions left
+        # ! because this method searches for something that might not be there,
+        # ! the traversal_path ( self.move_player(direction) ) can't be updated until we find a '?'
         return None
 
     def dft(self):
-        # * returns None when a dead-end is reached
+        # *     moves player in depth-first traversal of maze
+        # *     returns None when a dead-end is reached
+        s = Stack()
+        s.push(player.current_room.id)
+        while s.size() > 0:
+            curr_room = s.pop()
+            if curr_room not in self.visited:
+                self.visited.add(curr_room)
+                if curr_room not in self.rooms:
+                    self.add_room(curr_room)
+                self.add_unexplored_exits()
+                rand_dir = self.get_rand_unexplored_dir()
+                if rand_dir is None:
+                    return None
+                self.move_player(rand_dir)
+                new_room = player.current_room.id
+                self.update_exit(curr_room, rand_dir, new_room)
+                s.push(new_room)
+
         return None
 
     def traverse(self):
-        # * runs dft() until None is returned
-        # *     runs find_nearest_unexplored() once dft() returns None, then re-runs dft()
+        # * runs dft() until None is returned, then
+        # *     runs find_nearest_unexplored(), then re-runs traverse()
         # *     if find_nearest_unexplored returns None, 
-        # *         return None from this method
-        return None
+        # *         return None from this method, we are finished
+        exploring = self.dft()
+        if exploring is None:
+            print('we hit a dead end, retracing steps')
+            unexplored = self.find_nearest_unexplored()
+            if unexplored is None:
+                print('done')
+                return None
+            self.traverse()
 
-
+attempt = Traversal_Graph()
+attempt.traverse()
 
 # ! ---------------------End Ben Rogers Code-----------
 
