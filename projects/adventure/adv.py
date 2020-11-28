40f96c09990b7eeb187f1725f89ba50757092676
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-11-28 13:25:58 -0600

    Cleans up commenting, tightens up naming

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -37,21 +37,22 @@ class Traversal_Graph():
         self.rooms = {}
         self.visited = set()
 
-    def add_room(self, room_id):
+    def add_empty_room(self, room_id):
         self.rooms[room_id] = {}
         return None
 
     def show_exits(self, room_id):
-        # * returns dictionary
+        # *     returns dictionary
         return self.rooms[room_id]
 
     def move_player(self, cardinal_direction):
+        # *     keeps player.current_room.id and traversal_path in lockstep with each other
         player.travel(cardinal_direction)
         traversal_path.append(cardinal_direction)
         return None
 
     def get_opposite(self, cardinal_direction):
-        # * for use in self.update_exit()
+        # *     for use in self.update_exit()
         opposite = {
             'n': 's',
             's': 'n',
@@ -61,9 +62,9 @@ class Traversal_Graph():
         return opposite[cardinal_direction]
 
     def add_unexplored_exits(self):
-        #   player must be currently in the room 
-        # ! assumes that room is being visited for the 1st time
-        #   this function adds ALL exits, 
+        # *     player must be currently in the room 
+        # *     this function adds ALL available exits (will overwrite any existing entries)
+        # !     assumes that room is being visited for the 1st time
         room_id = player.current_room.id
         anon_exits = player.current_room.get_exits()
         for ex in anon_exits:
@@ -72,10 +73,10 @@ class Traversal_Graph():
         return None
 
     def update_exit(self, rm1_id, direction, rm2_id):
-        # ! call this function AFTER player has traveled to new unexplored exit
+        # !     call this function AFTER player has traveled to new unexplored exit
         self.rooms[rm1_id][direction] = rm2_id
         if rm2_id not in self.visited:
-            self.add_room(rm2_id)
+            self.add_empty_room(rm2_id)
         opposite_dir = self.get_opposite(direction)
         self.rooms[rm2_id][opposite_dir] = rm1_id
         return None
@@ -83,19 +84,19 @@ class Traversal_Graph():
     def get_rand_unexplored_dir(self):
         # *     returns a string with one cardinal direction from players CURRENT ROOM
         # *     returns None if dead-end
-        rand_arr = []
+        rand_list = []
         room_id = player.current_room.id
-        for direction, value in self.rooms[room_id].items():
+        for direction, value in self.show_exits(room_id).items():
             if value == '?':
-                rand_arr.append(direction)
-        if len(rand_arr) == 0:
+                rand_list.append(direction)
+        if len(rand_list) == 0:
             return None
-        return random.choice(rand_arr)
+        return random.choice(rand_list)
 
     def retrace(self, path_list):
-        # * moves player along path to nearest unexplored exit
+        # *     moves player along path to nearest unexplored exit
         for entry in path_list:
-            direction, rm = entry
+            direction, _ = entry
             self.move_player(direction)
         return None
 
@@ -103,12 +104,18 @@ class Traversal_Graph():
         # *     uses bfs to find path to nearest unexplored exit
         # *     once path is found, calls self.retrace() to move player there.
         # *         returns True once unexplored direction is found, or
-        # *     returns None if there are no unexplored directions left
+        # *         returns None if there are no unexplored directions left
         # *
-        # * each entry in the Queue holds lists of tuples (direction_to_travel, origin_room)
+        # *     we need the room we're traveling to so we can self.show_exits(curr_room),
+        # *     so each entry in the Queue holds lists of tuples: 
+        # *         (direction_to_travel, room_traveling_to)
+        # *         the room_traveling_to is ignored by self.retrace(curr_path)
 
         q = Queue()
         visited = set()
+
+        # *     this starting value is a throwaway tuple that kickstarts the while loop
+        # *     it is removed in the target-case check
         q.enqueue([(random.choice(player.current_room.get_exits()), player.current_room.id)])
 
         while q.size() > 0:
@@ -116,23 +123,24 @@ class Traversal_Graph():
             curr_node = curr_path[-1]
             curr_dir, curr_room = curr_node 
 
-            # # ? check for target here ?
             if curr_room == '?':
-                # removes the last direction (so self.dft() can manage selecting a new unexplored direction)
+                # *     removes the last direction 
+                # *     (so self.dft() can manage selecting a new unexplored direction)
                 curr_path.pop()
-                # removes the first direction (our path TO the starting Node, enqueue'd above)
+                # *     removes the first direction
+                # *     (our path TO the starting Node, enqueue'd above)
                 curr_path.pop(0)
-                # sends a proper list of tuples for self.retrace() to unpack
+                # *     sends a proper list of tuples for self.retrace() to unpack
                 self.retrace(curr_path)
                 return True
             
             if curr_node not in visited:
                 visited.add(curr_node)
 
-                # get the exits
+                # *     get the exits
                 exits = self.show_exits(curr_room)
 
-                # iterate over exits, enqueue the PATH to them
+                # *     iterate over exits, enqueue the PATH and ROOM to them
                 for key, value in exits.items():
                     path_copy = curr_path + [(key, value)]
                     q.enqueue(path_copy)
@@ -149,7 +157,7 @@ class Traversal_Graph():
             if curr_room not in self.visited:
                 self.visited.add(curr_room)
                 if curr_room not in self.rooms:
-                    self.add_room(curr_room)
+                    self.add_empty_room(curr_room)
                 self.add_unexplored_exits()
             rand_dir = self.get_rand_unexplored_dir()
             if rand_dir is None:
@@ -158,14 +166,13 @@ class Traversal_Graph():
             new_room = player.current_room.id
             self.update_exit(curr_room, rand_dir, new_room)
             s.push(new_room)
-
         return None
 
     def traverse(self):
-        # * runs dft() until None is returned, then
-        # *     runs find_nearest_unexplored(), then re-runs traverse()
-        # *     if find_nearest_unexplored returns None, 
-        # *         return None from this method, we are finished
+        # *     runs dft() until None is returned, then
+        # *         runs find_nearest_unexplored(), then re-runs traverse()
+        # *         if find_nearest_unexplored returns None, 
+        # *             return None from this method, we are finished
         exploring = self.dft()
         if exploring is None:
             unexplored = self.find_nearest_unexplored()
@@ -173,6 +180,7 @@ class Traversal_Graph():
                 return None
             self.traverse()
 
+# *     This attempt currently traverses the entire graph in 990-1028 moves
 attempt = Traversal_Graph()
 attempt.traverse()
 
